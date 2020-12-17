17c8a18e8ea678d865df309d1dda453e8d89710c
Jeff Johnson <jeff@gridfinity.com>
2020-12-17 05:51:43 -0500

    cov: Reformat to appease shellcheck all + shfmt

--- cov_report.sh
+++ cov_report.sh
@@ -13,24 +13,26 @@
 
 # Abort and inform in the case of csh or tcsh as sh.
 # shellcheck disable=SC2046,SC2006,SC2116,SC2065
-test _$(echo asdf 2>/dev/null) != _asdf >/dev/null &&
+test _$(echo asdf 2> /dev/null) != _asdf > /dev/null &&
 	printf '%s\n' "Error: csh as sh is unsupported." &&
 	exit 1
 
-cleanUp() {
+cleanUp()
+{
 	printf '%s\n' "Running cleanup tasks." >&2 ||
 		true :
-	set +u >/dev/null 2>&1 ||
+	set +u > /dev/null 2>&1 ||
 		true :
-	set +e >/dev/null 2>&1 ||
+	set +e > /dev/null 2>&1 ||
 		true :
-	rm -f ./gocov_report_gonuma* >/dev/null 2>&1 ||
+	rm -f ./gocov_report_gonuma* > /dev/null 2>&1 ||
 		true :
 	printf '%s\n' "All cleanup tasks completed." >&2 ||
 		true :
 }
 
-global_trap() {
+global_trap()
+{
 	err=${?}
 	trap - EXIT
 	trap '' EXIT INT TERM ABRT ALRM HUP
@@ -40,10 +42,9 @@ trap 'global_trap $?' EXIT
 trap 'err=$?; global_trap; exit $?' ABRT ALRM HUP TERM
 trap 'err=$?; trap - EXIT; global_trap $err; exit $err' QUIT
 trap 'global_trap; trap - INT; kill -INT $$; sleep 1; trap - TERM; kill -TERM $$' INT
-trap '' EMT IO LOST SYS URG >/dev/null 2>&1 ||
+trap '' EMT IO LOST SYS URG > /dev/null 2>&1 ||
 	true :
 
-set -o pipefail >/dev/null 2>&1
 if [ ! -f "./.gonuma_root" ]; then
 	printf '\n%s\n' "You must run this tool from the root directory" >&2
 	printf '%s\n' "of your local gonuma source tree or checkout." >&2
@@ -61,7 +62,7 @@ export GOC_TARGETS="$(go list ./... |
 	sort |
 	uniq)"
 
-type gocov 1>/dev/null 2>&1
+type gocov 1> /dev/null 2>&1
 # shellcheck disable=SC2181
 if [ "${?}" -ne 0 ]; then
 	printf '\n%s\n' "This script requires the gocov tool." >&2
@@ -84,10 +85,10 @@ cleanUp ||
 	# shellcheck disable=SC2086,SC2248
 	go test -v ${TEST2_TAGS:?${unset:?}} ${TEST_FLAGS:?${unset:?}} -bench=. ${GOC_TARGETS} -coverprofile gocov_report_gonuma2.profile |
 		grep --line-buffered -v '^coverage: ' &&
-		gocovmerge gocov_report_gonuma1.profile gocov_report_gonuma2.profile >gocov_report_gonuma.profile &&
+		gocovmerge gocov_report_gonuma1.profile gocov_report_gonuma2.profile > gocov_report_gonuma.profile &&
 		/bin/rm -f -- ./gocov_report_gonuma1.profile ./gocov_report_gonuma2.profile &&
-		gocov convert gocov_report_gonuma.profile >gocov_report_gonuma.json &&
-		gocov report <gocov_report_gonuma.json >gocov_report_gonuma.txt
+		gocov convert gocov_report_gonuma.profile > gocov_report_gonuma.json &&
+		gocov report < gocov_report_gonuma.json > gocov_report_gonuma.txt
 ) ||
 	{
 		printf '\n%s\n' "gocov failed complete successfully." >&2
@@ -95,7 +96,7 @@ cleanUp ||
 			:
 	}
 
-type gocov-html 1>/dev/null 2>&1
+type gocov-html 1> /dev/null 2>&1
 # shellcheck disable=SC2181
 if [ "${?}" -ne 0 ]; then
 	printf '%\n%s\n' "This script optionally utilizes gocov-html." >&2
@@ -104,7 +105,7 @@ if [ "${?}" -ne 0 ]; then
 	exit 1 ||
 		:
 fi
-(gocov-html <gocov_report_gonuma.json >gocov_report_gonuma.html) ||
+(gocov-html < gocov_report_gonuma.json > gocov_report_gonuma.html) ||
 	{
 		printf '\n%s\n' "gocov-html failed to complete successfully." >&2
 		exit 1 ||
